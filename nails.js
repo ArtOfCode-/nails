bd1571422033fae8ea87757e56c1295b08449f64
ArtOfCode <owen.jenkins2903@gmail.com>
2017-03-23 16:47:44 +0000

    Bugfix: Use config, not config.set

--- nails.js
+++ nails.js
@@ -2,5 +2,7 @@ var Server = require("./server.js").Server;
 var Config = require("./config.js").Config;
 
 exports.nails = appRoot => {
-  new Server(new Config(appRoot + "/config.json").set('app_root', appRoot)).run();
+  var cfg = new Config(appRoot + "/config.json");
+  cfg.set('appRoot', appRoot);
+  new Server(cfg).run();
 };
