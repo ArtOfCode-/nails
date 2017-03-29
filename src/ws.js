41beb18753dffe97f536186d92bc98afa2ae9462
J F <git@twopointzero.us>
2017-03-29 14:08:31 -0400

    Initial websocket handler

---
+++ src/ws.js
@@ -0,0 +1,8 @@
+const socket = require('socket.io');
+
+module.exports = exports = function (server, socketOptions) {
+  const io = socket(server, Object.assign({
+    path: '/ws'
+  }, socketOptions));
+  return io;
+}
