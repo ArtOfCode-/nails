6beed820c008eab6ad2cc4457444e675ecaf8fe4
Jed Fox <git@twopointzero.us>
2017-03-30 06:21:42 -0400

    Pacify XO II

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
-const {Router} = require('..');
+const { Router } = require('..');
 
-module.exports = Router.draw(({scope}) => {
-  scope('status', ({get}) => {
+module.exports = Router.draw(({ scope }) => {
+  scope('status', ({ get }) => {
     get('', 'status');
     get('param-switching', 'paramSwitching');
     get('json');
