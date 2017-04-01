be0c0c70dea7fa8144e82c4e34e6e06cdd3b3f0d
Jed Fox <git@twopointzero.us>
2017-04-01 12:24:38 -0400

    Whitespace in tests

--- test/test.js
+++ test/test.js
@@ -34,35 +34,43 @@ describe('server', () => {
   before(() => {
     server = nails({ appRoot: serverPath, start: false });
   });
+
   it('renders plain text', () => testServer(server, '/status').then(res => {
     assert.equal(res._getString(), 'status ok');
     assertContentType(res, 'text/plain');
   }));
+
   it('renders JSON', () => testServer(server, '/status/json').then(res => {
     assert.deepEqual(res._getJSON(), { this: 'renders', as: 'json' });
     assertContentType(res, 'application/json');
   }));
+
   it('renders a view', () => testServer(server, '/status/view').then(res => {
     assert.equal(res._getString(), '<h1>hi</h1>\n');
     assertContentType(res, 'text/html');
   }));
+
   it('redirects', () => testServer(server, '/status/redirect').then(res => {
     assert.equal(res.statusCode, 302);
     assert.equal(res.getHeader('location'), '/status');
     assert.equal(res._getString(), '');
   }));
+
   describe('renders 404 pages as', () => {
     const fourOhFourPath = path.join(serverPath, 'static', '404.html');
+
     it('HTML if 404.html is present', () => testServer(server, '/blah').then(res => {
       assert.equal(res._getString(), fs.readFileSync(fourOhFourPath, 'utf8'));
       assertContentType(res, 'text/html');
     }));
+
     it('static text if 404.html is not present', () => fs.rename(fourOhFourPath, fourOhFourPath + '.bak').then(() => {
       return testServer(server, '/blah');
     }).then(res => {
       assert.equal(res._getString(), 'Not Found');
       assertContentType(res, 'text/plain');
     }));
+
     after(() => fs.rename(fourOhFourPath + '.bak', fourOhFourPath));
   });
 });
