8d3d79647d73c591ee80a2e7e9028df7327d814a
Jed Fox <git@twopointzero.us>
2017-04-14 09:32:48 -0400

    Test nails.genKey()

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const assert = require('assert');
 
-process.env.DEBUG = process.env.DEBUG || "-*";
-const { before, describe } = require('mocha');
+process.env.DEBUG = process.env.DEBUG || "-nails, nails:test,nails:test:*";
+const debug = require('debug')('nails:test');
+const { before, describe, it } = require('mocha');
 
 const nails = global.NAILS_TEST_EXPORT = require('..');
 
@@ -11,3 +13,13 @@ const server = nails({ appRoot: serverPath, start: false });
 before(() => server.handler.ready);
 
 describe('HTTP', () => require('./http')({ server, serverPath }));
+describe('genKey', () => {
+  let key;
+  it('runs succesfully', () => {
+    key = nails.genKey();
+    debug('key:', key);
+  });
+  it('is 50 characters long', () => assert.equal(key.length, 50));
+  it('only contains allowed characters', () => assert.ok(key.match(/^[a-z0-9!@#$%^&*(-_=+)]+$/)));
+  it('allows specifying the length', () => assert.equal(nails.genKey(20).length, 20));
+});
